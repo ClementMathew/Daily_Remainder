6f08e4a8ed8f9c19ad52c9ceef15f4b12cccb6f1
ClementMathew <clementmathew924@gmail.com>
2024-08-26 23:49:51 +0530

    Daily Remainder web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "9cfd83625941446bc894bee176661bd1",
+"flutter_bootstrap.js": "31196ceb2fe61bf1d6427387dd47defc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
